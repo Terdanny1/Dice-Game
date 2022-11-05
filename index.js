979447ff36aaa7dc703653b45519dc442d5cee7e
Terdanny1 <terdanconcept68@gmail.com>
2022-11-05 22:14:07 +0100

    add a querySelectorAll and setAttribute for image1/player1

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1 //creates random number be
 
 var randomDiceImage = "dice" + randomNumber1 +   ".png" //string from dice1.png - dice6.png
 
-var randomImageSource = "images/" + randomDiceImage //images/dice1.png ...
+var randomImageSource = "images/" + randomDiceImage //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0]
+
+image1.setAttribute("src", randomImageSource)
