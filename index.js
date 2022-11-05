f69861a254d94d5d210b1a3d75adaa3969929f73
Terdanny1 <terdanconcept68@gmail.com>
2022-11-05 22:06:45 +0100

    declare a randomImageSource variable

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1 //creates random number between 1-6
 
 var randomDiceImage = "dice" + randomNumber1 +   ".png" //string from dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage //images/dice1.png ...
