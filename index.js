328d834ad6aa0d1995c3cd8c34f05a6eadbfa0f0
Terdanny1 <terdanconcept68@gmail.com>
2022-11-05 22:22:49 +0100

    declare a variable for the second random number and randomImageSource

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ var randomDiceImage = "dice" + randomNumber1 +   ".png" //string from dice1.png 
 var randomImageSource = "images/" + randomDiceImage //images/dice1.png - images/dice6.png
 
 var image1 = document.querySelectorAll("img")[0]
-
 image1.setAttribute("src", randomImageSource)
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1 //creates random number between 1-6
+
+var randomImageSource2 = "images/dice" + randomNumber2 +  ".png"
+
