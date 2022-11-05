5270d31ec29a3cd34449851447b103e0a3021e77
Terdanny1 <terdanconcept68@gmail.com>
2022-11-05 22:38:15 +0100

    add conditionals

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1 //creates random number be
 var randomImageSource2 = "images/dice" + randomNumber2 +  ".png"
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2)
+
+
+// if player1 wins
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!"
+}
+// if player2 wins
+else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!"
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
